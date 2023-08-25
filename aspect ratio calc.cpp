e376e5e4cd46b3d9d04a84fe48e6d44938ce9b25
EmreHero <120343813+EmreHero@users.noreply.github.com>
2023-08-25 19:13:18 +0300

    .Cpp file

---
+++ aspect ratio calc.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+
+int main() {
+	
+	std::cout << "Coded by EmreHero~\n\n";
+	
+	int width, height;
+	
+	std::cout << "Width?\n>> ";
+	std::cin >> width;
+	std::cout << "Height?\n>> ";
+	std::cin >> height;
+	
+	for(int i = 2; i <= height && i <= width; i++) {
+		
+		if (width%i == 0 && height % i == 0) {
+			
+			width /= i;
+			height /= i;
+		}
+	}
+	
+	std::cout << "Aspect ratio is " << width << ":" << height << std::endl;
+	
+	system("pause");
+}
